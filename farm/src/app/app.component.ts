4a7165f3f6e99c4ca4def30f5876fe0032bb9f82
unknown <s.sharma087@gmail.com>
2018-06-08 21:31:31 +0530

    add pop-up on explorebutton

--- farm/src/app/app.component.ts
+++ farm/src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  onActivate(event) {
+    window.scroll(0,0);
+    //or document.body.scrollTop = 0;
+    //or document.querySelector('body').scrollTo(0,0)
+    
 }
+}
+
